fce7788336090ba06c8dfd2c67c854e222e5935f
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-08-16 00:58:01 +0300

    Добавлен код для принта текста

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 const data = [
-    'hello world'
-]
+    'hello world',
+    'data data\ndata data data data\ndata data data data data data\ndata data data\ndata data data data data data\ndata data data data\ndata data data data data '
+];
+
+const config = {
+    printTime: 10,
+};
 
 
 const paragraphTemplate = document.querySelector('#screen-paragraph-template').content.querySelector('.screen-paragraph');
 const screenElem = document.querySelector('.screen');
 
-function addParagraph (screen, str) {
-    const p = paragraphTemplate.cloneNode();
-    p.textContent = str;
-    screen.append(p)
+function addSymbol (paragraph, symbol) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            if (symbol === '\n') symbol = '<br>';
+            paragraph.innerHTML += symbol;
+            resolve();
+        }, config.printTime);
+    });
 }
 
-addParagraph(screenElem, data[0])
+async function addParagraph (screen, str) {
+    const p = paragraphTemplate.cloneNode();
+    screen.append(p);
+    if (p.previousElementSibling) p.previousElementSibling.classList.remove('screen-paragraph_cursor');
+    for (let c of str) {
+        await addSymbol(p, c);
+    }
+}
 
+addParagraph(screenElem, data[1]);
